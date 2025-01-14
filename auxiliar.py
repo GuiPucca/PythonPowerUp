6ef285439c28fb29915b3bab3a7c3af7e9b495e9
Guilherme Pucca <guilherme.pucca@gmail.com>
2025-01-14 18:58:23 -0300

    tudo pronto!

---
+++ auxiliar.py
@@ -0,0 +1,14 @@
+import pyautogui
+
+import time
+
+time.sleep(5)
+
+print(pyautogui.position())
+
+
+# x=394, y=410 -> meu perfil
+# x=588, y=324 -> outro perfil
+# x=418, y=306 -> posição do primeiro item do relatório
+
+
--- codigo.py
+++ codigo.py
@@ -1,4 +1,10 @@
 import pyautogui
+import time
+import pandas
+
+pyautogui.PAUSE = 0.8
+
+# Passo 1: Abrir o sistema da empresa
 
 pyautogui.press("Win")
 
@@ -6,4 +12,75 @@ pyautogui.write("Chrome")
 
 pyautogui.press("Enter")
 
-pyautogui.click()
+pyautogui.click(x=588, y=324)
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+
+pyautogui.press("Enter")
+
+time.sleep(0.5)
+
+# Passo 2: Fazer login
+
+pyautogui.press("Tab")
+
+pyautogui.write("pythonteste@python.com")
+
+pyautogui.press("Tab")
+
+pyautogui.write("pythonsenha")
+
+pyautogui.press("Enter")
+
+time.sleep(1.2)
+
+# Passo 3: Importar Banco de Dados
+
+tabela = pandas.read_csv("produtos.csv")
+
+for linha in tabela.index:
+    pyautogui.click(x=418, y=306)  # clica no primeiro campo
+
+    # Codigo
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+    pyautogui.press("Tab")
+
+    # Marca
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+    pyautogui.press("Tab")
+
+    # Tipo
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+    pyautogui.press("Tab")
+
+    # Categoria
+    categoria = tabela.loc[linha, "categoria"]
+    pyautogui.write(str(categoria))
+    pyautogui.press("Tab")
+
+    # Preço unitario
+    preco = tabela.loc[linha, "preco_unitario"]
+    pyautogui.write(str(preco))
+    pyautogui.press("Tab")
+
+    # Custo
+    custo = tabela.loc[linha, "custo"]
+    pyautogui.write(str(custo))
+    pyautogui.press("Tab")
+
+    # Observação
+    obs = str(tabela.loc[linha, "obs"])
+    if obs != "nan":
+        pyautogui.write(obs)
+    pyautogui.press("Tab")
+
+    pyautogui.press("Enter")
+
+    pyautogui.scroll(+1000)
+
+
+
+
